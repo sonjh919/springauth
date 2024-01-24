efef27c5c0880ecb9821f36c0180d209b2ebb542
Son JunHyung <kingjh1125@gmail.com>
2024-01-25 01:12:30 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/springauth/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/springauth/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.sparta.springauth.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,21 +12,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // 인가 처리, 옵션 설정 등 수행
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
         http.csrf((csrf) -> csrf.disable());
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
             authorizeHttpRequests
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 폴더 접근 허용 설정
-//                .requestMatchers("/api/user/**").permitAll() // 허용 설정, 다양한 케이스들이 있음
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 로그인 사용
-        http.formLogin(Customizer.withDefaults()); // security에서 default login 기능 제공 // id : user, password : 실행될때마다 달라짐
-        // session id가 security 내부에 저장되어 인증 처리를 해줌
-        // security는 기본적으로 session 방식으로 작동된다.
+        http.formLogin((formLogin) ->
+            formLogin
+                // 로그인 View 제공 (GET /api/user/login-page)
+                .loginPage("/api/user/login-page")
+                // 로그인 처리 (POST /api/user/login)
+                .loginProcessingUrl("/api/user/login")
+                // 로그인 처리 후 성공 시 URL
+                .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
+                .failureUrl("/api/user/login-page?error")
+                .permitAll()
+        );
 
         return http.build();
     }
---
+++ src/main/java/com/sparta/springauth/security/UserDetailsImpl.java
@@ -0,0 +1,67 @@
+package com.sparta.springauth.security;
+
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+// UserDetails랑 UserDetailsService를 커스텀하면 디폴트 로그인 기능을 사용하지 않겠다는 설정
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 권한 설정 - 접근 불가 페이지 만들 때
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
